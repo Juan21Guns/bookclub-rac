ee929ebcebf0a2ae82f7b0fe72fbde160281bc77
Vaughn Schneider <reelyvaughn@gmail.com>
2023-07-28 21:12:39 -0600

    fully working now

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 
 function App() {
+  axios.get('http://0.0.0.0:80/working', { withCredentials: false })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
   return (
     <div className="App">
       <p>This is still working</p>
